36189297a7d2e2803adb5e78066d0bdace7ed3dc
Philip-webdev <philisobank@gmail.com>
2026-02-17 02:37:03 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+    optimizeDeps: {
+    include: ["three"],
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+}
 }));
